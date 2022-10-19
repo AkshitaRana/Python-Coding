0467964b245502668e6452d531029cb5c1f32ddb
AkshitaRana <68386442+AkshitaRana@users.noreply.github.com>
2022-10-19 22:11:33 +0530

    Create Solve me first.py

---
+++ Solve me first.py
@@ -0,0 +1,12 @@
+#Python hacker rank question
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    sum1=a+b
+    return(sum1)
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
