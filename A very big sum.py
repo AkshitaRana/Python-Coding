63859bd37b571e6c337365cbf34827356bad33e5
AkshitaRana <68386442+AkshitaRana@users.noreply.github.com>
2022-10-19 22:10:19 +0530

    Create A very big sum.py

---
+++ A very big sum.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+# Python hacker rank question
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    sum1=0
+    for num in ar:
+        sum1+=num
+    return(sum1)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
