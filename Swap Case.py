e61b78373b1109e44b881515021c039eb97eaf81
AkshitaRana <68386442+AkshitaRana@users.noreply.github.com>
2022-10-28 22:10:32 +0530

    Create Swap Case.py

---
+++ Swap Case.py
@@ -0,0 +1,12 @@
+# PYTHON HACKER RANK QUESTION
+
+def swap_case(s):
+    string=""
+    for i in s:
+        if i.isupper():
+            string+=i.lower()
+        elif i.islower():
+            string+=i.upper()    
+        else:
+            string+=i
+    return string
