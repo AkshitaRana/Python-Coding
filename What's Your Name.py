1e623c0de458247b2bcf93aebfddee80321f00cb
AkshitaRana <68386442+AkshitaRana@users.noreply.github.com>
2022-11-05 21:46:36 +0530

    Create What's Your Name.py

---
+++ What's Your Name.py
@@ -0,0 +1,11 @@
+# PYTHON HACKER RANK QUESTION
+
+
+def print_full_name(first, last):
+    f=first
+    l=last
+    print("Hello {} {}! You just delved into python.".format(f,l))
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
